f3fbda388a782e65a1677de2e40c9151458468fe
WaiShen1000 <91667994+WaiShen1000@users.noreply.github.com>
2022-04-20 22:06:23 +0800

    Add files via upload

---
+++ Arduino.ino
@@ -0,0 +1,28 @@
+void setup() {
+  pinMode(13, OUTPUT); //Drone power switch
+  digitalWrite(13, HIGH);
+  pinMode(6, OUTPUT); //Electromagnet relay
+  pinMode(7, OUTPUT); //Charge relay
+  pinMode(8, INPUT); //IR sensor
+
+}
+
+void loop() {
+ if(digitalRead(8) == LOW)
+ {
+  delay(2000);
+  digitalWrite(13, LOW);
+  delay(200);
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  delay(2700000);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  delay(2000);
+  digitalWrite(13, LOW);
+  delay(200);
+  digitalWrite(13, HIGH);
+ }
+}
